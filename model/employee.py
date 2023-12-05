d437d77d07f81a2007ee3f23e367546fba28e1b4
Sverrir Björgvinsson <sverrirp@gmail.com>
2023-12-05 11:34:15 +0000

    Adding info for model/employee.py

--- model/employee.py
+++ model/employee.py
@@ -1,5 +1,13 @@
 class Employee:
-    def __init__(self, name="", gsm="", address=""):
-            self.name = name
-            self.gsm = gsm
-            self.address = address
+    def __init__(self, name="", national_id="", address="", gsm="", email=""):
+        self.name = name
+        self.national_id = national_id
+        self.address = address
+        self.gsm = gsm
+        self.address = address
+        self.email = email
+
+    nafn, kennitala, heimilisfang, gsm - sími, netfang
+
+    def __str__(self):
+        return f"name: {self.name}, national id: {self.national_id}, address: {self.address}, mobile number: {self.gsm}, email: {self.email}"
