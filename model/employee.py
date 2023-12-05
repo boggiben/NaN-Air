73b142ee1f76995267a79abf80c6ea8d30276cbd
Sverrir Björgvinsson <sverrirp@gmail.com>
2023-12-05 11:37:42 +0000

    adding staff role to model/employee.py

--- model/employee.py
+++ model/employee.py
@@ -1,11 +1,12 @@
 class Employee:
-    def __init__(self, name="", national_id="", address="", gsm="", email=""):
+    def __init__(self, name="", national_id="", address="", gsm="", email="", role=""):
         self.name = name
         self.national_id = national_id
         self.address = address
         self.gsm = gsm
         self.address = address
         self.email = email
+        self.role = role
 
     def __str__(self):
-        return f"name: {self.name}, national id: {self.national_id}, address: {self.address}, mobile number: {self.gsm}, email: {self.email}"
+        return f"name: {self.name}, national id: {self.national_id}, address: {self.address}, mobile number: {self.gsm}, email: {self.email}, role: {self.role}"
