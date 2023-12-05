dfda36c8433afd54ea36c0c61b4c9fc7da53b8a0
styxstrider <stigur.sigurdarson@gmail.com>
2023-12-05 12:40:39 +0000

    Update staff_logic.py

--- logic/staff_logic.py
+++ logic/staff_logic.py
@@ -1,4 +1,5 @@
 from model.employee import Employee
+import csv
 
 class Staff_Logic:
     def __init__(self, data_wrapper):
@@ -8,7 +9,10 @@ class Staff_Logic:
         pass
         
     def see_all_staff(self):
-        return [Employee()]
+        with open("staff.csv", newline="") as csvfile:
+            read = csv.DictReader(csvfile)
+            for row in read:
+                 print(row["first_name"], row["last_name"])
 
     def add_new_flight(self, plane, employee):
         pass
