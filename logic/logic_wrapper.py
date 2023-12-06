6365ef0b4184217c2a5518fe27024e4c2f5cbd15
bjartursg <bjartursol@gmal.com>
2023-12-06 11:53:44 +0000

    breytt create new destination UI

--- logic/logic_wrapper.py
+++ logic/logic_wrapper.py
@@ -8,27 +8,27 @@ class LogicWrapper:
         self.data_wrapper = DataWrapper()
         self.staff_logic = Staff_Logic(self.data_wrapper)
         self.destination_logic = DestinationLogic(self.data_wrapper)
-        
+
     def get_all_flights(self):
         pass
 
     def get_flights_by_week(self):
         pass
-    
+
     def get_all_staff(self):
         self.staff_logic.see_all_staff()
-        
+
     def add_new_flight(self):
         pass
-    
+
     def add_staff_to_flight(self):
         pass
-    
+
     def add_new_staff(self):
         pass
-    
+
     def get_all_destinations(self):
         return self.data_wrapper.get_all_destinations()
-    
-    def create_destination(self, destination):
+
+    def add_new_destination(self, destination):
         return self.data_wrapper.create_destination
