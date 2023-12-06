7ae985db9d07761a0c39014ce8b55f0ee3099b78
bjartursg <bjartursol@gmal.com>
2023-12-06 11:28:40 +0000

    destination breyting

--- UI/destination_ui.py
+++ UI/destination_ui.py
@@ -6,21 +6,21 @@ from logic.logic_wrapper import LogicWrapper
 class Destination_UI:
     def __init__(self, logic_connection):
         self.logic_wrapper = logic_connection
-        
+
     def destination_menu(self):
         print("Áfangastaðir")
         print("1. Stofna nýjan áfangastað")
         print("2. Sjá alla áfangastaði")
         print("b. Til baka")
-    
+
     def input_destination(self):
         while True:
-            self.destination_menu()    
+            self.destination_menu()
             action = input("Veldu aðgerð: ")
-            
-            
+
             if action == "1":
-                self.logic_wrapper.add_new_destination()
+                # self.logic_wrapper.add_new_destination()
+                self.create_destination_ui()
             elif action == "2":
                 self.logic_wrapper.get_all_destinations()
             elif action == "b":
@@ -29,3 +29,19 @@ class Destination_UI:
             else:
                 print("Rangur innsláttur")
                 self.destination_menumenu()
+
+    def create_destination_ui(self):
+        destination_list = []
+        country = input("Land: ")
+        destination_list.append(country)
+        airport = input("Flugvöllur: ")
+        destination_list.append(airport)
+        duration = input("Flugtími: ")
+        destination_list.append(duration)
+        distance = input("Fjarlægð: ")
+        destination_list.append(distance)
+        contact = input("Nafn tengiliðs: ")
+        destination_list.append(contact)
+        number = input("Neyðarnúmer tengiliðs: ")
+        destination_list.append(number)
+        self.logic_wrapper.add_new_destination(destination_list)
