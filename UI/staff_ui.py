ee567ab2adbfc093f61234559cfd5a7facca10e1
styxstrider <stigur.sigurdarson@gmail.com>
2023-12-06 11:29:08 +0000

    Update staff_ui.py

--- UI/staff_ui.py
+++ UI/staff_ui.py
@@ -1,5 +1,3 @@
-from logic.logic_wrapper import LogicWrapper
-
 class Staff_UI:
     def __init__(self, logic_connection):
         self.logic_wrapper = logic_connection
@@ -18,7 +16,7 @@ class Staff_UI:
             user_input = input("Veldu aðgerð: ")
             if user_input == "1":
                 print("Þú valdir að sjá alla starfsmenn")
-                self.logic_wrapper.get_all_staff
+                
             elif user_input.lower() == "b":
                 break
             else:
