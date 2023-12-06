5644a3e69d3eba80940a39a6035ca6bf625e3dea
Sverrir Bjorgvinsson <sverrirp@gmail.com>
2023-12-06 04:34:52 +0000

    Minor changes to staff_ui and main_menu_ui

--- UI/main_menu_ui.py
+++ UI/main_menu_ui.py
@@ -1,4 +1,5 @@
 from logic.logic_wrapper import LogicWrapper
+from ui.staff_ui import Staff_UI
 
 
 class MainMenu_UI:
@@ -8,10 +9,10 @@ class MainMenu_UI:
     def menu_output(self):
         print("Velkominn á aðalvalmynd")
         print("1. Skoða öll skráð flug")
-        print("2. Skoða flug vikunar")
+        print("2. Skoða flug vikunnar")
         print("3. Listi yfir starfsmenn")
         print("4. Skrá nýja flugferð")
-        print("5 Skrá starfsmann í vinnuferð")
+        print("5. Skrá starfsmann í vinnuferð")
         print("6. Skrá nýjan starfsmann")
         print("q. Hætta")
         print("*" * 30)
@@ -39,7 +40,9 @@ class MainMenu_UI:
             elif user_input == "2":
                 self.logic_wrapper.get_flights_by_week()
             elif user_input == "3":
-                self.logic_wrapper.get_all_staff()
+                menu = Staff_UI(self.logic_wrapper)
+                menu.input_staff_menu()
+                # self.logic_wrapper.get_all_staff()
             elif user_input == "4":
                 self.logic_wrapper.add_new_flight()
             elif user_input == "5":
--- UI/staff_ui.py
+++ UI/staff_ui.py
@@ -1,3 +1,22 @@
-class Staff_ui:
-    def __init__(self):
-        print()
+class Staff_UI:
+    def __init__(self, logic_connection):
+        self.logic_wrapper = logic_connection
+
+    def menu_output(self):
+        print("--Starfsmenn--")
+        print("1. Sjá alla starfsmenn")
+        print("2. Skrá nýjan starfsmann")
+        print("3. Sjá lausa starfsmenn")
+        print("4. Sjá upptekna starfsmenn")
+        print("b. Til að fara í aðalvalmynd")
+
+    def input_staff_menu(self):
+        while True:
+            self.menu_output()
+            user_input = input("Veldu aðgerð: ")
+            if user_input == "1":
+                print("Þú valdir að sjá alla starfsmenn")
+            elif user_input.lower() == "q":
+                break
+            else:
+                print("Rangur innsláttur. Reyndu aftur.")
