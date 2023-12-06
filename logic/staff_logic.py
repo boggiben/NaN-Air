075dcb1475c8ac5baf8441f2d340c2eed387b2f5
Viktor Sigurðsson <viktorsig9@gmail.com>
2023-12-06 10:35:57 +0000

    removed data code fro stafflogic

--- logic/staff_logic.py
+++ logic/staff_logic.py
@@ -1,14 +1,13 @@
 from model.employee import Employee
 import csv
+
 file_name = "files/staff.csv"
+
 
 class Staff_Logic:
     def __init__(self, data_wrapper):
         self.data_wrapper = data_wrapper
 
-    def add_new_staff(self, employee):
-        pass
-        
     def see_all_staff(self):
         staff_list = []
         with open(file_name, newline="") as csvfile:
@@ -21,11 +20,27 @@ class Staff_Logic:
     def add_new_flight(self, plane, employee):
         pass
 
+    def add_new_staff(self, employee):
+        with open(file_name, newline="") as csvfile:
+            fieldnames = [
+                "Nafn",
+                "Símanúmer",
+                "Netfang",
+                "Heimilisfang",
+                "kennitala",
+                "Staða",
+            ]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
     def add_staff_to_flight(self, employee):
         pass
 
-    def get_all_flights(self, ):
+    def get_all_flights(
+        self,
+    ):
         pass
 
-    def get_flights_by_week(self,):
-        pass 
+    def get_flights_by_week(
+        self,
+    ):
+        pass
