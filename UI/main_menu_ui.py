06371c2901bfe23e6068cf97efaa886f64a57242
Sverrir Bjorgvinsson <sverrirp@gmail.com>
2023-12-06 14:46:19 +0000

    Changes to ui/main_menu_ui.py

--- UI/main_menu_ui.py
+++ UI/main_menu_ui.py
@@ -12,9 +12,9 @@ class MainMenu_UI:
         print(f"{'Velkomin/n á aðalvalmynd':^75}")
         print("*" * 80)
 
-        print(f"{'1. Skoða öll skráð flug':<80}")
-        print(f"{'2. Skoða flug vikunnar':<80}")
-        print(f"{'3. Starfsmenn':<80}")
+        print(f"{'1. Starfsmenn':<80}")
+        print(f"{'2. Áfangastaðir':<80}")
+        print(f"{'3. Flugferðir':<80}")
         print(f"{'4. Skrá nýja flugferð':<80}")
         print(f"{'5. Skrá starfsmann í vinnuferð':<80}")
         print(f"{'6. Skrá nýjan starfsmann':<80}")
@@ -30,13 +30,14 @@ class MainMenu_UI:
             user_input = input("Veldu aðgerð: ")
             user_input = user_input.lower()
             if user_input == "1":
-                self.logic_wrapper.get_all_flights()
-            elif user_input == "2":
-                self.logic_wrapper.get_flights_by_week()
-            elif user_input == "3":
                 menu = Staff_UI(self.logic_wrapper)
                 menu.input_staff_menu()
                 # self.logic_wrapper.get_all_staff()
+            elif user_input == "2":
+                menu = Destination_UI(self.logic_wrapper)
+                menu.input_destination()
+            elif user_input == "3":
+                self.logic_wrapper.get_all_flights()
             elif user_input == "4":
                 self.logic_wrapper.add_new_flight()
             elif user_input == "5":
@@ -44,8 +45,7 @@ class MainMenu_UI:
             elif user_input == "6":
                 self.logic_wrapper.add_new_staff()
             elif user_input == "7":
-                menu = Destination_UI(self.logic_wrapper)
-                menu.input_destination()
+                self.logic_wrapper.get_flights_by_week()
             elif user_input == "q":
                 print("Bless")
                 break
