af5ed0bd0ef233c5429392b6b9137646ea5e9d51
Viktor Sigurðsson <viktorsig9@gmail.com>
2023-12-06 11:11:55 +0000

    put the read_staff in the data

--- data/data_wrapper.py
+++ data/data_wrapper.py
@@ -1,19 +1,20 @@
 from data.staff_data import Staff_Data
 from data.destination_data import DestinationData
 
+
 class DataWrapper:
     def __init__(self):
         self.staff_data = Staff_Data()
         self.destination_data = DestinationData()
-        
+
     def create_staff(self, employee):
         self.staff_data.create_staff(employee)
-        
+
     def get_all_staff(self):
-        pass
-    
+        return self.staff_data.read_all_staff()
+
     def get_all_destinations(self):
         return self.destination_data.get_all_destinations()
-    
+
     def create_destination(self, destination):
         return self.destination_data.create_destination(destination)
--- logic/logic_wrapper.py
+++ logic/logic_wrapper.py
@@ -6,23 +6,21 @@ class LogicWrapper:
     def __init__(self):
         self.data_wrapper = DataWrapper()
         self.staff_logic = Staff_Logic(self.data_wrapper)
-        
+
     def get_all_flights(self):
         pass
 
     def get_flights_by_week(self):
         pass
-    
+
     def get_all_staff(self):
-        self.staff_logic.see_all_staff()
-        
+        self.staff_logic.get_all_staff()
+
     def add_new_flight(self):
         pass
-    
+
     def add_staff_to_flight(self):
         pass
-    
+
     def add_new_staff(self):
         pass
-    
-    
